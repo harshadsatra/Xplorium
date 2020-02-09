1d8c0b08968955dc6b1128ea994e00602ac8e84a
Harshad <harshad.satra2@gmail.com>
2020-02-09 16:11:19 +0530

    Content Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,8 +11,23 @@ $('.accordian-btn').click(function() {
   }
 });
 
+$(window).on('load', function(){
+  console.log('loader');
+  headerLoadAnim();
+});
 
 
+function headerLoadAnim(){
+  // $('.hiddenBlock').addClass('active');
+  var tl = new TimelineMax();
+    tl.to($('#headerTitle'), 0.4, {opacity:1, ease:Power1.easeInOut})
+    tl.to($('#headerTitle .part2'), 0.4, {opacity:1, ease:Power1.easeInOut})
+    tl.to($('#headerTitle'), 2, {opacity:0, ease:Power1.easeInOut})
+    tl.to($('#logo'), 2, {opacity:1, ease:Power1.easeInOut})
+    tl.set($("#main-header"),{className:"-=main-header"})
+    tl.set($("body"),{className:"-=loaded"});
+    $(window).scrollTop(0);
+};
 
 
 $('#popup-btn').on('click',function(e){
@@ -32,9 +47,33 @@ $(function(){
   $('#scrollActivator').scrollStory({
     debug: false,
     content: '.trigger',
-    triggerOffset: '35%',
+    triggerOffset: '45%',
     itemfocus: function(ev, item){
-      console.log('item active', item.index, item.id, item.data, item);
+      console.log('item active', item.index, item.id, item.data, item.type, item.option);
+      var item = item.data;
+      if(item.type == 'heading'){
+        $('.bg-content').hide();
+        console.log('heading');
+        if(item.option == 'show1'){
+          $(item.id).addClass('active');
+        }
+        if(item.option == 'show2'){
+          $(item.id+' span').addClass('active');
+        }
+        if(item.option == 'hide2'){
+          $(item.id).removeClass('active');
+        }
+        if(item.option == 'hide1'){
+          $(item.id+' span').removeClass('active');
+        }
+      }
+
+      if(item.type == 'content'){
+        $('.bg-content').hide();
+        $(item.id).fadeIn();
+      }
+      /*
+
       if(item.id === 't1'){
         console.log('Activating trigger 1');
       } else {
@@ -52,6 +91,7 @@ $(function(){
       } else {
         console.log('Deactivating trigger 3');
       }
+      */
       
     }
   });
